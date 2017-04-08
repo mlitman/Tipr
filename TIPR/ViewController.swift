b32735468e066d3923a1cb5e839989ab42f29ce2
Mike Litman <awesomefat@gmail.com>
2017-04-08 08:17:56 -0500

    Initial Commit

---
+++ TIPR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TIPR
+//
+//  Created by Michael Litman on 4/8/17.
+//  Copyright © 2017 thinkful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
